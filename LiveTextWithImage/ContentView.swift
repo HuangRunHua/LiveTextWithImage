3e851bcccb83e0267362f1dc1555dc53104a9477
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-06-09 19:58:03 +0800

    Initial Commit

---
+++ LiveTextWithImage/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LiveTextWithImage
+//
+//  Created by Runhua Huang on 2022/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveTextWithImage/LiveTextWithImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTextWithImageApp.swift
+//  LiveTextWithImage
+//
+//  Created by Runhua Huang on 2022/6/9.
+//
+
+import SwiftUI
+
+@main
+struct LiveTextWithImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
